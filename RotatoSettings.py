55bf6e54fa1940bf91e0e4ed245faeb92ef49937
Joshua Martin <joshuamartintx@gmail.com>
2025-10-17 13:31:57 -0500

    two files

---
+++ RotatoSettings.py
@@ -0,0 +1,14 @@
+
+# program timing settings
+timeLimit = 30 * 1000
+updateTick = 1
+roundRobinQuant = 100
+
+# motors
+rot180Degree_time = 2.5
+rot180Degree_power = 0.5
+moveSideways_power = 0.5
+rotGradual_power = 0.5
+
+# color detection settings
+colorThreshold = 0.5
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import threading
+import time
+from collections import deque
+import RotatoSettings
+
+class task:
+    def __task__(self, name, quantTime):
+        self.name = name
+        self.quantTime = quantTime
+        self.remainingTime = quantTime
+
+class redTask(task):
+    pass 
+class greenTask(task):
+    pass 
+class blueTask(task):
+    pass 
 
 def classifyColor(r, g, b, shareThreshold) -> int:
     # red = 1, green = 2, blue = 3, none = 0
@@ -14,7 +31,7 @@ def classifyColor(r, g, b, shareThreshold) -> int:
     else:
         return 0
 
-def colorCounter(image, imageDimX, imageDimY, shareThreshold) -> dict:
+def ColorCounter(image, imageDimX, imageDimY, shareThreshold) -> dict:
     # red = 1, green = 2, blue = 3, none = 0
     colorDict = {
         0: 0,
@@ -30,3 +47,59 @@ def colorCounter(image, imageDimX, imageDimY, shareThreshold) -> dict:
 
     return colorDict
 
+def ColorLocator(image, imageDimX, imageDimY, shareThreshold, color_counts):
+    colorType = max(color_counts, key=color_counts.get)
+    averageX = 0.0
+    averageY = 0.0
+    numPoint = 0.0
+
+    for i in range(imageDimY):
+        for j in range(imageDimX):
+            pixel = image[i, j]
+            if classifyColor(pixel[0], pixel[1], pixel[2], shareThreshold) is colorType:
+                numPoint += 1
+                averageX += j
+                averageY += i
+
+    averageX /= numPoint
+    averageY /= numPoint
+
+    return tuple(averageX, averageY)
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+def main() :
+    # --- Setup --- #
+    currTime = 0.0
+    lastTick = 0.0
+
+    # -- update loop --- #
+    taskQueue = deque(task)
+    tasksInQueue = {} # dict to keep track of queued tasks
+    while currTime < RotatoSettings.timeLimit:
+        currTime = current_milli_time()
+
+        # --- per-update functions --- #
+        # read camera
+        # threshold color
+
+        # if color threshold achieved and task not in queue, insert task into queue; make sure to add bookkeeping to tasksInQueue
+        # otherwise do nothing
+
+        # --- Round-robin --- #
+        while taskQueue:
+            currTime
+        # if no task currently executing, pop next task from the queue and execute
+        # when time exceeds, move to next task (maybe reschedule task if it does not complete in time?)
+        # if no tasks, do nothing
+
+    # --- end --- #
+
+    pass
+
+
+if __name__ == "__main__":
+    print("Starting program")
+    main()
+    print("Ending program")
